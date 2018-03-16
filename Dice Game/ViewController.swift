9c0a41c8bac1a1ce62d8e6165b20c188548a4d0d
Anubhav <anubhavmalikdeveloper@gmail.com>
2018-03-17 00:41:26 +0530

    Initial Commit

--- Dice Game/ViewController.swift
+++ Dice Game/ViewController.swift
@@ -6,10 +6,21 @@
 //  Copyright © 2018 Anubhav Malik. All rights reserved.
 //
 
+
+
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myFirstDiceImageView: UIImageView!
+    @IBOutlet weak var mySecondDiceImageView: UIImageView!
+    @IBOutlet weak var diceText: UITextField!
+    
+    var randomDiceNumber = 0
+    var randomDiceNumberTwo  = 0
+    
+    let diceArray = ["Dice1","Dice2","Dice3","Dice4","Dice5","Dice6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +31,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func rollButtonClicked(_ sender: Any) {
+        
+      updateDiceImages()
+        
+    }
+    func updateDiceImages (){
+        randomDiceNumber = Int(arc4random_uniform(6))
+        
+        randomDiceNumberTwo = Int(arc4random_uniform(6))
+        
+        print(randomDiceNumber)
+        
+        myFirstDiceImageView.image = UIImage(named : diceArray[randomDiceNumber])
+        mySecondDiceImageView.image = UIImage(named : diceArray[randomDiceNumberTwo])
+    }
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
 }
 
